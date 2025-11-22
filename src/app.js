15220d280feff96adf086fab66d1749f6c136b26
joselucas-coder <sousajoselucas10@gmail.com>
2025-11-22 16:20:28 -0300

    Ajustes nos arquivos app.js e auth.js

--- src/app.js
+++ src/app.js
@@ -1,17 +1,15 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const originalRoutes = require('./routes/routes'); 
 const animalRoutes = require('./routes/animalPerdidoEncontrado');
 const animalAdocao = require('./routes/animalAdocao');
 const authRoutes = require('./routes/auth');
-
 const app = express();
 
 app.use(express.json());
-
+app.use('/perdidos/animais-perdidos', animalRoutes);
 app.use('/auth', authRoutes);
 app.use(originalRoutes); 
-app.use(animalRoutes);
-app.use(animalAdocao);
+app.use(animalAdocao); 
 
 module.exports = app;
